0b22d3c62a76a4c3b4f5d745828dcfdfa1407187
richardstolen <richstolen@gmail.com>
2023-02-16 15:41:33 +0100

    :sparkles: Started on css for App.js (background)

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-function App() {
-  const apiURL = "https://rs-lost-in-translation-api-production.up.railway.app";
-  const apiKey =
-    "uaxHSSnPmAp7LlijwMDezv2WXYbg1zx25YSdlzdB7zrOc83KvXE09y2ueIm5QCtV";
+import React from "react";
+import "./App.css";
 
-  fetch(`${apiURL}/translations`, {
-    method: "POST",
-    headers: {
-      "X-API-Key": apiKey,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      username: "mega-mind",
-      translations: [],
-    }),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Could not create new user");
-      }
-      console.log(response.json());
-    })
-    .then((newUser) => {
-      // newUser is the new user with an id
-    })
-    .catch((error) => {});
-  return <div className="App"></div>;
+function App() {
+  return (
+    <div
+      className="
+    App"
+    >
+      <div id="header"></div>
+    </div>
+  );
 }
 
 export default App;
