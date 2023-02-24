909452b971237480735ce124e115c89a444dc21f
richardstolen <richstolen@gmail.com>
2023-02-24 10:40:02 +0100

    :sparkles: Added extension to loginpage import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { useUser } from "./components/UserContext";
 import TranslationPage from "./pages/Translation/TranslationPage";
-import LoginPage from "./pages/Login/LoginPage";
+import LoginPage from "./pages/Login/LoginPage.jsx";
 import Logout from "./components/Logout";
 import ProfilePage from "./pages/Profile/ProfilePage";
 import "./App.css";
