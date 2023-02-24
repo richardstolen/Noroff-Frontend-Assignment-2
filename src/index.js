f086e50ff09095ce7b9da74ab276b5ef2365acb7
richardstolen <richstolen@gmail.com>
2023-02-24 11:29:28 +0100

    :bug: Trying out hash router instead of browser router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import UserProvider from "./components/UserContext";
 
@@ -8,9 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <UserProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </UserProvider>
   </React.StrictMode>
 );
