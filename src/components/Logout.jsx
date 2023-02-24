61aa7ffe83641d30f8d4727c4b39b2f6f3d7fc88
richardstolen <richstolen@gmail.com>
2023-02-24 11:11:41 +0100

    :bug: Added a link in logout component

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useUser } from "./UserContext";
 
 function Logout() {
@@ -10,7 +11,7 @@ function Logout() {
     sessionStorage.clear();
   });
 
-  return <div>Logged out</div>;
+  return <Link to="/"></Link>;
 }
 
 export default Logout;
